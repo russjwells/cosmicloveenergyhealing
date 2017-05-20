8ed2d7de9e22f3c14f13a2aed917c4dbe4218003
Russ Wells <russ@russdigital.com>
2017-05-19 19:54:50 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Cosmic Love Energy Healing</h2>
+        </div>
+        <p className="App-intro">
+          Its like reiki, only stronger :)
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
