dd5ea1fe55f19fc64f8fa6926d9d1b98277c817e
Russ Wells <russ@russdigital.com>
2018-07-01 16:13:32 -0700

    updates summer 2018

--- src/App.js
+++ src/App.js
@@ -10,16 +10,54 @@ class App extends Component {
           <h2>Cosmic Love Energy Healing</h2>
         </div>
         <p className="App-intro">
-          Welcome to Cosmic Love Energy Healing!
-          <br /> <br />
-          My name is Russ. I am an energy worker and intuitive healer, self taught. I heal with energy, crystals, herbs, plants, essential oils, sound, shamanic journeying, and Love.
+          Hello. I am Russell Wells and I am here to help. 
         </p>
-        <p>
-          I am taking new clients. If you are interested in healing together please send me a message at 513 - 332 - 5252.
+        <p className="body-copy">
+          I am a self taught energy worker and intuitive healer.
         </p>
-        <p>
-          Healing is $60/Hour or $1/Minute for one session. Prepaid packages of sessions are available at a discount.
+        <p className="body-copy">
+        I am also initiated into the healing art of Reiki in the Usui lineage.
         </p>
+        <p className="body-copy">
+          I am passionate about helping others stand in their fullest expression.
+        </p>
+        <p className="body-copy">
+           I am offering 30, 60, 90, and 120 minute energy work sessions.
+        </p>
+        <p className="body-copy">
+          Lay on the table and relax as your energetic system is supported and restored.
+        </p>
+        <p className="body-copy">
+          I am based in Ocean Beach, San Diego, California. 
+        </p>
+        <p className="body-copy">
+        I have spaces open for a few weekly clients.
+        </p>
+        <p className="body-copy">
+          New to energy work? 
+        </p>
+        <p className="body-copy">
+         Let's connect and discover how I can be of service to your growth.
+        </p>
+        <p className="body-copy">
+          I am on <a href="http://www.facebook.com/cosmicloveenergyhealing" className="bodylink">facebook</a> and <a href="http://www.instagram.com/cosmicloveenergyhealing" className="bodylink">instagram</a>.
+        </p>
+        
+        <p className="body-copy">
+          My telephone is 760-936-1704.
+        </p>
+        <p className="body-copy">
+          I am here to serve the light inside of you.
+        </p>
+        <p className="body-copy">
+           &nbsp;
+        </p>
+        <p className="body-copy">
+          Russell
+        </p>
+        <div className="App-footer">
+          <h2><a href="http://www.calendly.com/cosmicloveenergyhealing" className="headerlink">click here to book me</a></h2>
+        </div>
       </div>
     );
   }
