30fdfebaea7b9a510d3459469292160e8ba17346
Russ Wells <russ@russdigital.com>
2018-07-01 20:58:23 -0700

    page improvements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import starbg from './bluegreenstarfield.png';
+import cosmiclove from './cosmiclove.png';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
+          <img src={cosmiclove} className="App-logo" alt="logo" />
           <h2>Cosmic Love Energy Healing</h2>
         </div>
         <p className="App-intro">
